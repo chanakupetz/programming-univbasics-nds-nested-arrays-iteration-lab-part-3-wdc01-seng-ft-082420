8cc94540db496ab6118e8c96bd116931cd40d698
Chana Kupetz <chanakupetz@gmail.com>
2020-07-29 19:54:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
 words = []
 row_index = 0
 
-while row_index < src[row_index].count do
+while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if element_index.class == String
